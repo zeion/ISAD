2bfa9ac48924c1d012c7b889a7b1cec7f3d44cfd
Trithep Ratanapipob <the_prince_of_nightmare@hotmail.co.jp>
2015-04-22 07:31:41 +0700

    calendar feed ข้อมูลได้แล้วววว

--- ProjectISAD/src/java/data/DataCalendar.java
+++ ProjectISAD/src/java/data/DataCalendar.java
@@ -5,14 +5,16 @@
  */
 package data;
 
+import com.google.gson.Gson;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.*;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.json.simple.*;
+import model.CalendarDTO;
 
 
 /**
@@ -36,31 +38,63 @@ public class DataCalendar extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
+
             
-            JSONObject json = new JSONObject();
-            JSONArray addresses = new JSONArray();
-            JSONObject address;
-            String jsonString = "[";
-            try {
-                int count = 5;
-
-                for (int i = 0; i < count; i++) {
-                    address = new JSONObject();
-                    jsonString +=   "{start:"+"\"2015-04-1"+i+"\""+
-//                                    ",id:"+"\""+i+"0\""+
-                                    ",title:"+"\"Event"+i+"\"}";
+            
+            
+            
+            
+            
+            
+            
+            
+            
+//            JSONObject json = new JSONObject();
+//            JSONArray addresses = new JSONArray();
+//            JSONObject address;
+//            
+//            
+//            try {
+//
+//                int count = 5;
+//
+//                for (int i = 0; i < count; i++) {
+//                    address = new JSONObject();
+//                    address.put("CustomerName", "Decepticons" + i);
+//                    address.put("AccountId", "1999" + i);
+//                    address.put("SiteId", "1888" + i);
+//                    address.put("Number", "7" + i);
+//                    address.put("Building", "StarScream Skyscraper" + i);
+//                    address.put("Street", "Devestator Avenue" + i);
+//                    address.put("City", "Megatron City" + i);
+//                    address.put("ZipCode", "ZZ00 XX1" + i);
+//                    address.put("Country", "CyberTron" + i);
 //                    addresses.add(address);
-                    if(count-1!=i){
-                        jsonString += ",";
-                    }
-                }
-                jsonString += "]";
-//                json.put("event", addresses);
-            } catch (Exception jse) {
+//                }
+//                json.put("Addresses", addresses);
 
-            }
-            response.setContentType("application/json");
-            response.getWriter().write(jsonString);
+//                int count = 5;
+//
+//                for (int i = 0; i < count; i++) {
+//                    address = new JSONObject();
+//                    jsonString +=   "{start:"+"\"2015-04-1"+i+"\""+
+////                                    ",id:"+"\""+i+"0\""+
+//                                    ",title:"+"\"Event"+i+"\"}";
+////                    addresses.add(address);
+//                    if(count-1!=i){
+//                        jsonString += ",";
+//                    }
+//                }
+//                jsonString += "]";
+//                json.put("event", addresses);
+//            } catch (Exception jse) {
+//
+//            }
+//            response.setContentType("application/json");
+//            response.setCharacterEncoding("UTF-8");
+//            response.getWriter().write(addresses.toString());
+
+//            response.getWriter().write(jsonString);
         }
     }
 
@@ -76,8 +110,28 @@ public class DataCalendar extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
-    }
+        List l = new ArrayList();
+
+        CalendarDTO c = new CalendarDTO();
+        c.setId(1);
+        c.setStart("2015-04-15");
+        c.setEnd("2015-05-29");
+        c.setTitle("ติดแล้วววว");
+
+        CalendarDTO d = new CalendarDTO();
+        d.setId(2);
+        d.setStart("2015-03-26");
+        d.setEnd("2015-04-20");
+        d.setTitle("โว้ยยยยยยยยยย");
+
+        l.add(c);
+        l.add(d);
+
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter out = response.getWriter();
+        out.write(new Gson().toJson(l));
+    }    
 
     /**
      * Handles the HTTP <code>POST</code> method.
---
+++ ProjectISAD/src/java/model/CalendarDTO.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Admin
+ */
+public class CalendarDTO {
+    public int id;
+    public String title;
+    public String start;
+    public String end;
+    public String color;
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getStart() {
+        return start;
+    }
+    public void setStart(String start) {
+        this.start = start;
+    }
+    public String getEnd() {
+        return end;
+    }
+    public void setEnd(String end) {
+        this.end = end;
+    }
+}
